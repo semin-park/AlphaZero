12c132b6368ca348332fcd17f8745c3d59a83c14
Semin Park <SPark9625@gmail.com>
2019-04-28 15:18:19 +0900

    Training with GPU

--- src/train.cpp
+++ src/train.cpp
@@ -43,6 +43,10 @@ void get_data()
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 2)
+        throw std::runtime_error("Specify the current step in unit of thousand. E.g., ./train 17 (=17,000)");
+
+    int step = atoi(argv[1]) * 1000 + 1;
     int buffer_save_target = 500;
     int buffer_save_increment = 500;
 
@@ -57,14 +61,17 @@ int main(int argc, char const *argv[])
     int board_size = env.get_board_size();
     
     NetConfig& netconf = NetConfig::get();
+    auto device = torch::kCPU;
 
     PVNetwork net(board_size, netconf.resblocks(), c_in, netconf.channels(), c_out);
+    std::string path = load_network(net);
+
     if (torch::cuda::is_available()) {
         std::cout << "Using CUDA" << std::endl;
-        net->to(torch::kCUDA);
+        device = torch::kCUDA;
+        net->to(device);
     }
 
-    std::string path = load_network(net);
     torch::optim::Adam optimizer(net->parameters(), torch::optim::AdamOptions(1e-5).beta1(0.9).beta2(0.999));
 
     std::string dir = "replay";
@@ -76,19 +83,22 @@ int main(int argc, char const *argv[])
     try {
         torch::Tensor state, policy, reward;
         torch::Tensor p, v;
-        for (int i = 1; ; i++) {
+        for (; ; step++) {
             if (!alive)
                 break;
 
             net->zero_grad();
             std::tie(state, policy, reward) = buffer.get_batch(batch_size);
 
+            state = state.to(device);
+            policy = policy.to(device);
+            reward = reward.to(device);
             
             std::tie(p, v) = net(state);
 
             torch::Tensor vloss = torch::sum(torch::pow(v - reward, 2)) / 2;
             torch::Tensor ploss = -torch::sum(torch::log(p) * policy);
-            torch::Tensor wloss = torch::zeros({});
+            torch::Tensor wloss = torch::zeros({}).to(device);
             for (auto& param : net->parameters())
                 wloss += torch::norm(param, 2);
             
@@ -98,13 +108,11 @@ int main(int argc, char const *argv[])
 
             optimizer.step();
 
-            if (i % 100 == 0)
-                std::cout << "Step " << i << " | loss: " << loss.item<float>() << "\r" << std::flush;
-            if (i % 1000 == 0)
+            if (step % 100 == 0)
+                std::cout << "Step " << step << " | loss: " << loss.item<float>() << "\r" << std::flush;
+            if (step % 5000 == 0) {
                 path = save_network(net, path);
-            if (buffer.size() > buffer_save_target) {
                 buffer.save(dir);
-                buffer_save_target += buffer_save_increment;
                 std::cout << "Buffer saved. Size: " << buffer.size() << std::endl;
             }
         }
